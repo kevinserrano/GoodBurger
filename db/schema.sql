11777c6b4f49e1078120b48d600786afc8b50b94
kevinserrano <66225834+kevinserrano@users.noreply.github.com>
2020-10-16 12:56:11 -0500

    added layout for the schema file

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+    id INT NOT NULL
+    AUTO_INCREMENT PRIMARY KEY,
+    burgers_name VARCHAR
+    (60),
+    devoured BOOLEAN NOT NULL
+  );
