90de34250946f0d8d6f842b1e3869f8e1784518b
kevinserrano <66225834+kevinserrano@users.noreply.github.com>
2020-10-16 11:27:21 -0500

    added dependencies to server file along with mysql PW using .env

---
+++ server.js
@@ -0,0 +1,7 @@
+// Dependencies
+var express = require("express");
+var exphbs = require("express-handlebars");
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+require("console.table")
+require("dotenv").config();
