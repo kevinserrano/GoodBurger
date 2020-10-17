b7fa4b791bd81a432823942b2f5fb5d55ed33bd2
kevinserrano <66225834+kevinserrano@users.noreply.github.com>
2020-10-16 20:23:45 -0500

    added handlebars sntax

--- server.js
+++ server.js
@@ -5,3 +5,22 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 require("console.table")
 require("dotenv").config();
+
+var port = process.env.PORT || 3306;
+
+
+
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handelbars");
+
+
+
+
+
+
+
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+  
