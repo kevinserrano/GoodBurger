2ed31c83cfa003d787bfcdadf8deb5fc336da69e
kevinserrano <66225834+kevinserrano@users.noreply.github.com>
2020-10-20 18:57:09 -0500

    added routes for post get and put

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,8 +1,28 @@
-  
 var express = require("express");
-var burgers = require("./models/burgers");
 
+var router = express.Router();
 
+var burger = require("../models/burger.js");
 
+router.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+        res.render("index", { burgers: data });
+    });
+});
 
+router.post("/api/burgers", function(req, res) {
+    burger.insertOne([req.body.name], function(result) {
+        res.json({ id: result.insertId });
+    });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+    let burgerId = req.params.id;
+    burger.updateOne(burgerId, function(result) {
+        if (result.changedRows === 0) {
+            return res.status(404).end();
+        }
+        res.status(200).end()
+    })
+})
 module.exports = router;
