4de12aa7301058d37f10c82a5af2dd8b3d331e2a
kevinserrano <66225834+kevinserrano@users.noreply.github.com>
2020-10-20 18:52:46 -0500

    starting from scratch and taking a right approach

--- server.js
+++ server.js
@@ -1,35 +1,22 @@
-// Dependencies
-var express = require("express");
-var mysql = require("mysql");
-
+const express = require('express');
 const exphbs = require("express-handlebars");
+const router = require('./controllers/burgers_controller.js');
 
-const connection = require("./config/connection");
 const app = express();
-const PORT = process.env.PORT || 3006;
+const PORT = process.env.PORT || 3000;
 
+// Use the express.static middleware to serve static content for the app from the "public" directory in the application directory.
+// not sure this is needed --- check later 
+app.use(express.static("public"));
+
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.use(router);
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-
-
-app.get("/", function(req,res) {
-  connection.query("SELECT * FROM burgers", function (err,data){
-    if (err) throw err;
-    res.render("index", {burgers: data});
-  });
-});
-
-app.post("/", function(req, res) {
-  connection.query("INSERT INTO burgers (burgers_name) VALUES (?)", [req.body.burgers_name], function(err, result) {
-    if (err) throw err;
-
-    res.redirect("/");
-  });
-});
-
 
 
 
