1e8dc95d319b540f6d86273e765cc993aeec2ff4
kevinserrano <66225834+kevinserrano@users.noreply.github.com>
2020-10-20 23:37:33 -0500

    added better html index

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,28 +1,13 @@
+  
 var express = require("express");
+var burgers = require("./models/burgers");
 
-var router = express.Router();
 
-var burger = require("../models/burger.js");
-
-router.get("/", function(req, res) {
-    burger.selectAll(function(data) {
-        res.render("index", { burgers: data });
+router.get("/", function(req,res) {
+    connection.query("SELECT * FROM burgers", function (err,data){
+      if (err) throw err;
+      res.render("index", {burgers: data});
     });
-});
+  });
 
-router.post("/api/burgers", function(req, res) {
-    burger.insertOne([req.body.name], function(result) {
-        res.json({ id: result.insertId });
-    });
-});
-
-router.put("/api/burgers/:id", function(req, res) {
-    let burgerId = req.params.id;
-    burger.updateOne(burgerId, function(result) {
-        if (result.changedRows === 0) {
-            return res.status(404).end();
-        }
-        res.status(200).end()
-    })
-})
 module.exports = router;
--- server.js
+++ server.js
@@ -1,29 +1,24 @@
-const express = require('express');
-const exphbs = require("express-handlebars");
-const router = require('./controllers/burgers_controller.js');
+  
+var express = require("express");
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 
-// Use the express.static middleware to serve static content for the app from the "public" directory in the application directory.
-// not sure this is needed --- check later 
+var app = express();
+
 app.use(express.static("public"));
 
-// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(router);
+var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+var routes = require("./controllers/burgers_controller.js");
 
-
-
+app.use(routes);
 
 app.listen(PORT, function() {
-    console.log("Server listening on: http://localhost:" + PORT);
-  });
-  module.exports = express;
-  
+    console.log("App now listening at localhost:" + PORT);
+});
